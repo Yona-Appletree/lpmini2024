ca036a9a7532ece6f072b204a88cc896543d7efc
Yona Appletree <hypher@gmail.com>
2025-05-10 21:00:34 -0700

    Initial commit

---
+++ visgen/src/App.tsx
@@ -0,0 +1,12 @@
+import VisPreview from "./components/VisPreview";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+      <VisPreview />
+    </div>
+  );
+}
+
+export default App;
