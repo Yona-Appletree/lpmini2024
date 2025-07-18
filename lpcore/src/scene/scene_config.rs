562f77f15d004a8300297df04fb0ba856ba3f173
Yona Appletree <hypher@gmail.com>
2025-07-18 15:57:31 -0700

    before node vs entity?

--- lpcore/src/scene/scene_config.rs
+++ lpcore/src/scene/scene_config.rs
@@ -1,6 +1,5 @@
 use crate::entities::EntityKind;
 use crate::expr::Expr;
-use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use serde_json::Value as JsonValue;
 use std::collections::HashMap;
@@ -8,50 +7,27 @@ use std::collections::HashMap;
 #[derive(Serialize, Deserialize, JsonSchema, Debug)]
 pub struct SceneConfig {
     pub name: String,
-    pub module: ModuleConfig,
+    pub nodes: HashMap<String, NodeConfig>,
+    // future: add support for scene-defined nodes
+    //pub node_defs: HashMap<String, NodeDef>,
 }
 
 #[derive(Serialize, Deserialize, JsonSchema, Debug)]
 pub struct NodeConfig {
-    pub kind: NodeKind,
+    ///
+    /// ID of the node definition to use for this node instance
+    ///
+    /// A URI-style identifier like:
+    /// - builtin:lfo
+    /// - scene:my_node
+    ///  
+    pub node_def_id: String,
 
     /// Raw input values for the node
     pub input: JsonValue,
 
     /// Map of input path "x.y.z" to expression to evaluate
     pub bindings: HashMap<String, Expr>,
-}
-
-/// Specification of the kind of node
-#[derive(Serialize, Deserialize, JsonSchema, Debug)]
-pub enum NodeKind {
-    Entity(EntityKind),
-    Module(String),
-}
-
-#[derive(Serialize, Deserialize, JsonSchema, Debug)]
-pub struct ModuleConfig {
-    pub name: Option<String>,
-
-    pub inputs: Option<HashMap<String, ModuleInput>>,
-    pub outputs: Option<HashMap<String, ModuleOutput>>,
-
-    pub nodes: HashMap<String, NodeConfig>,
-    pub modules: HashMap<String, ModuleConfig>,
-}
-
-#[derive(Serialize, Deserialize, JsonSchema, Debug)]
-pub struct ModuleInput {
-    pub name: String,
-    pub entity_id: String,
-    pub input_path: String,
-}
-
-#[derive(Serialize, Deserialize, JsonSchema, Debug)]
-pub struct ModuleOutput {
-    pub name: String,
-    pub entity_id: String,
-    pub output_path: String,
 }
 
 #[derive(Serialize, Deserialize, JsonSchema, Debug)]
