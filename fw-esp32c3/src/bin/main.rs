280a274c10cf8c407372f10908c8a5dfcb957e01
Yona Appletree <hypher@gmail.com>
2025-09-20 14:38:32 -0700

    first light with one led

--- fw-esp32c3/src/bin/main.rs
+++ fw-esp32c3/src/bin/main.rs
@@ -6,20 +6,21 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use esp_hal::clock::CpuClock;
-use esp_hal::timer::systimer::SystemTimer;
-use esp_hal::timer::timg::TimerGroup;
-use esp_wifi::ble::controller::BleConnector;
+extern crate alloc;
 use bt_hci::controller::ExternalController;
-
- use defmt::info;
-
+// use esp_wifi::ble::controller::BleConnector;
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-
+use esp_hal::clock::CpuClock;
+use esp_hal::rmt::Rmt;
+use esp_hal::time::Rate;
+use esp_hal::timer::systimer::SystemTimer;
+use esp_hal::timer::timg::TimerGroup;
+use esp_hal_smartled::{buffer_size_async, SmartLedsAdapterAsync};
 use panic_rtt_target as _;
-
-extern crate alloc;
+use smart_leds::hsv::{hsv2rgb, Hsv};
+use smart_leds::{brightness, gamma, SmartLedsWriteAsync, RGB8};
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -45,20 +46,62 @@ async fn main(spawner: Spawner) {
 
     let rng = esp_hal::rng::Rng::new(peripherals.RNG);
     let timer1 = TimerGroup::new(peripherals.TIMG0);
-    let wifi_init = esp_wifi::init(timer1.timer0, rng)
-        .expect("Failed to initialize WIFI/BLE controller");
-    let (mut _wifi_controller, _interfaces) = esp_wifi::wifi::new(&wifi_init, peripherals.WIFI)
-        .expect("Failed to initialize WIFI controller");
-    // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
-    let transport = BleConnector::new(&wifi_init, peripherals.BT);
-    let _ble_controller = ExternalController::<_, 20>::new(transport);
+
+    // Configure RMT (Remote Control Transceiver) peripheral globally
+    // <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/peripherals/rmt.html>
+    let rmt: Rmt<'_, esp_hal::Async> = {
+        let frequency: Rate = Rate::from_mhz(80);
+        Rmt::new(peripherals.RMT, frequency)
+    }
+    .expect("Failed to initialize RMT")
+    .into_async();
+
+    // We use one of the RMT channels to instantiate a `SmartLedsAdapterAsync` which can
+    // be used directly with all `smart_led` implementations
+    const num_leds: usize = 100;
+    let rmt_channel = rmt.channel0;
+    let rmt_buffer = [0_u32; buffer_size_async(num_leds)];
+
+    // Each devkit uses a unique GPIO for the RGB LED, so in order to support
+    // all chips we must unfortunately use `#[cfg]`s:
+    let mut led = SmartLedsAdapterAsync::new(rmt_channel, peripherals.GPIO4, rmt_buffer);
+
+    // let wifi_init = esp_wifi::init(timer1.timer0, rng)
+    //     .expect("Failed to initialize WIFI/BLE controller");
+    // let (mut _wifi_controller, _interfaces) = esp_wifi::wifi::new(&wifi_init, peripherals.WIFI)
+    //     .expect("Failed to initialize WIFI controller");
+    //
+    // // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
+    // let transport = BleConnector::new(&wifi_init, peripherals.BT);
+    // let _ble_controller = ExternalController::<_, 20>::new(transport);
+
+    let mut color = Hsv {
+        hue: 0,
+        sat: 255,
+        val: 255,
+    };
+    let mut data: RGB8;
+    let level = 10;
 
     // TODO: Spawn some tasks
     let _ = spawner;
 
     loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
+        for hue in 0..=255 {
+            color.hue = hue;
+            // Convert from the HSV color space (where we can easily transition from one
+            // color to the other) to the RGB color space that we can then send to the LED
+            data = hsv2rgb(color);
+
+            // When sending to the LED, we do a gamma correction first (see smart_leds
+            // documentation for details) and then limit the brightness to 10 out of 255 so
+            // that the output is not too bright.
+            led.write(brightness(gamma([data].into_iter()), level))
+                .await
+                .unwrap();
+
+            Timer::after(Duration::from_millis(10)).await;
+        }
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
