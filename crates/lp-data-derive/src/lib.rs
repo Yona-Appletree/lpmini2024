88487f10140f6fef9c6e00ce7d190a79878dc52d
Yona Appletree <hypher@gmail.com>
2025-11-07 16:40:00 -0800

    Fix clippy warnings in lp-data-derive
    - Remove unnecessary mut on input variable
    - Remove irrefutable if let pattern

--- crates/lp-data-derive/src/lib.rs
+++ crates/lp-data-derive/src/lib.rs
@@ -10,15 +10,12 @@ use syn::{parse_macro_input, DeriveInput};
 /// It also supports `#[lpschema(...)]` attributes that map to schemars metadata.
 #[proc_macro_derive(LpDataType, attributes(lpschema))]
 pub fn lp_data_type_derive(input: TokenStream) -> TokenStream {
-    let mut input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
 
     // Add JsonSchema to the existing derives
     // For now, we just ensure JsonSchema is derived - in the future we can process
     // #[lpschema(...)] attributes to map to schemars metadata
-    if let syn::Data::Struct(_) | syn::Data::Enum(_) | syn::Data::Union(_) = &input.data {
-        // The user should already have JsonSchema in their derive list
-        // This macro is mainly for future attribute processing
-    }
+    // All Data variants (Struct, Enum, Union) are handled the same way
 
     // Just pass through the input unchanged for now
     // Users should include JsonSchema in their derive list
